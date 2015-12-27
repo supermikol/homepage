873d580ac22051910056c01fc4c67c8edd0df413
supermikol <mdu123@gmail.com>
2015-12-26 17:12:56 -0800

    update site

--- assets/sidebar.js
+++ assets/sidebar.js
@@ -1,39 +1,25 @@
 $(document).ready(function() {
-  $('.sidelink1').mouseenter(function(){
-    $('.sidelink1').fadeTo('fast', 0.5);
+  $('.sidelink').mouseenter(function(){
+    $(this).fadeTo('fast', 0.5);
   });
-  $('.sidelink1').mouseleave(function() {
-    $('.sidelink1').fadeTo('fast', 1);
-  });
-    $('.sidelink2').mouseenter(function(){
-    $('.sidelink2').fadeTo('fast', 0.5);
-  });
-  $('.sidelink2').mouseleave(function() {
-    $('.sidelink2').fadeTo('fast', 1);
-  });
-    $('.sidelink3').mouseenter(function(){
-    $('.sidelink3').fadeTo('fast', 0.5);
-  });
-  $('.sidelink3').mouseleave(function() {
-    $('.sidelink3').fadeTo('fast', 1);
-  });
-  $('.sidelink4').mouseenter(function(){
-  $('.sidelink4').fadeTo('fast', 0.5);
-  });
-  $('.sidelink4').mouseleave(function() {
-    $('.sidelink4').fadeTo('fast', 1);
+  $('.sidelink').mouseleave(function() {
+    $(this).fadeTo('fast', 1);
   });
   $('#logo').mouseenter(function(){
-  $('#logo').fadeTo('fast', 0.8);
+  $(this).fadeTo('fast', 0.8);
   });
   $('#logo').mouseleave(function() {
-    $('#logo').fadeTo('fast', 1);
+    $(this).fadeTo('fast', 1);
   });
   $('#tagname').mouseenter(function(){
-  $('#tagname').fadeTo('fast', 0.5);
+  $(this).fadeTo('fast', 0.5);
   });
   $('#tagname').mouseleave(function() {
-  $('#tagname').fadeTo('fast', 1);
+  $(this).fadeTo('fast', 1);
+  });
+  $('.sidelink').focus(function(){
+    $(this).css('outline-style', 'solid');
+    $(this).css('outline-color', '#00FFFF');
   });
 });
 
